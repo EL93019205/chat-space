d5d6639bdedccb092871bbf9d127c43af58977c2
EL93019205 <59168351+EL93019205@users.noreply.github.com>
2020-04-11 15:00:14 +0900

    MessagesControllerTest_Step8

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -11,10 +11,13 @@ describe MessagesController do
       end
 
       it '@messageに期待した値が入っていること' do
+        expect(assigns(:message)).to be_a_new(Message)
       end
       it '@groupに期待した値が入っていること' do
+        expect(assigns(:group)).to eq group
       end
       it 'index.html.erbに遷移すること' do
+        expect(response).to render_template :index
       end
     end
     
@@ -23,6 +26,7 @@ describe MessagesController do
         get :index, params: { group_id: group.id }
       end
       it 'ログイン画面にリダイレクトすること' do
+        expect(response).to redirect_to(new_user_session_path)
       end
     end
   end
