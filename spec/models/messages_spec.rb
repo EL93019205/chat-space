f3ae626cb9d0254cc22eaaf4f16b5e155a040b00
EL93019205 <59168351+EL93019205@users.noreply.github.com>
2020-04-11 12:13:41 +0900

    MessagesModelTest_Step5

--- spec/models/messages_spec.rb
+++ spec/models/messages_spec.rb
@@ -3,9 +3,21 @@ require 'rails_helper'
 RSpec.describe Message, type: :model do
   describe '#create' do
     context 'messageを保存できる場合' do
+      it 'contentがあれば保存できること' do
+      end
+      it 'imageがあれば保存できること' do
+      end
+      it 'content と imageがあれば保存できること' do
+      end
     end
     
     context 'messageを保存できない場合' do
+      it 'contentとimageが両方空だと保存できないこと' do
+      end
+      it 'group_idが無いと保存できないこと' do
+      end
+      it 'user_idが無いと保存できないこと' do
+      end
     end
   end
 end
