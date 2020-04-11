fa2773da23e0ea3a1312c1c53bd71bbd6f0f9868
EL93019205 <59168351+EL93019205@users.noreply.github.com>
2020-04-11 14:59:38 +0900

    MessagesControllerTest_Step5

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -3,9 +3,17 @@ require 'rails_helper'
 describe MessagesController do
   describe '#create' do
     context 'ログインしている場合' do
+      it '@messageに期待した値が入っていること' do
+      end
+      it '@groupに期待した値が入っていること' do
+      end
+      it 'index.html.erbに遷移すること' do
+      end
     end
     
     context 'ログインしていない場合' do
+      it 'ログイン画面にリダイレクトすること' do
+      end
     end
   end
 end
