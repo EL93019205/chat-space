987389ef5f9708e9807a6791dc7ca011a86be445
EL93019205 <59168351+EL93019205@users.noreply.github.com>
2020-04-11 12:13:25 +0900

    MessagesModelTest_Step4

--- spec/models/messages_spec.rb
+++ spec/models/messages_spec.rb
@@ -2,6 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Message, type: :model do
   describe '#create' do
+    context 'messageを保存できる場合' do
+    end
+    
+    context 'messageを保存できない場合' do
+    end
   end
 end
 
