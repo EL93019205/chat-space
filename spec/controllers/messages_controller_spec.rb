85b4fe3ded79eb904ad2f8bac9581b403dbaf0ec
EL93019205 <59168351+EL93019205@users.noreply.github.com>
2020-04-11 14:59:50 +0900

    MessagesControllerTest_Step6

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 describe MessagesController do
+  let(:group) { create(:group) }
+  let(:user) { create(:user) }
   describe '#create' do
     context 'ログインしている場合' do
       it '@messageに期待した値が入っていること' do
