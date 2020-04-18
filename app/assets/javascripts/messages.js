85428c30b88c3e8ab6289d888e453d221be6452b
EL93019205 <59168351+EL93019205@users.noreply.github.com>
2020-04-18 17:28:01 +0900

    非同期通信成功時、DOMにHTMLを追加
    # What
    非同期通信成功時に、追加するHTMLを作成し、DOMに追加する処理を記述しました。
    このとき、画像つきであれば画像つき用のHTMLを作成するようにしました。
    
    # Why
    非同期通信で投稿を即座に反映するためです。

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,4 +1,25 @@
 $(function(){
+  function buildHTML(message){
+    var img_src = ``;
+    if( message.image ){
+      img_src = `<img src=${message.image}>`;
+    }
+    var html = `<div class="main-chat__message-list__message">
+                  <div class="main-chat__message-list__message__user-info">
+                    <div class="main-chat__message-list__message__user-info__name">
+                      ${message.user_name}
+                    </div>
+                    <div class="main-chat__message-list__message__user-info__time">
+                      ${message.created_at}
+                    </div>
+                  </div>
+                  <div class="main-chat__message-list__message__post-text">
+                    ${message.content}
+                  </div>
+                  ${img_src}
+                </div>`;
+    return html;
+  }
 
   $('#new_message').on('submit', function(e){
     e.preventDefault();
@@ -13,6 +34,8 @@ $(function(){
       contentType: false
     })
     .done(function(data){
+      var html = buildHTML(data);
+      $('.main-chat__message-list').append(html);
     })
     .fail(function(){
     })
